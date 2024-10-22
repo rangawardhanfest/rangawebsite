f8f90327d2670b26fef17db544453db10f864bc1
Harshala <mharshala334@gmail.com>
2024-10-23 01:14:59 +0530

    theme updated

--- src/pages/home/Theme/Theme.js
+++ src/pages/home/Theme/Theme.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactPlayer from "react-player";
 import "./Theme.css";
-import yashogatha from "../../img/yashogatha.png"
+
 const Theme = () => {
   return (
     <>
-    <h1 className="animate-charcter center" data-aos="fade-right" data-aos-duration="3000">यशोगाथा प्रतिभाशाली महाराष्ट्राची</h1>
+    <h1 className="animate-charcter center" data-aos="fade-right" data-aos-duration="3000">रौप्यमहोत्सव समृद्ध महाराष्ट्राच्या गौरवाचा</h1>
     <div className="itheme">
     <div className="i-left">
       <div className="i-left-wrapper">
-            <img className='centerimg' src={yashogatha}  data-aos="fade-right" data-aos-duration="2000" alt='' />
+            <img className='centerimg' src='https://res.cloudinary.com/dcir5216l/image/upload/v1729625742/Theme_logo_24-25-removebg_oxb3lg.png'  data-aos="fade-right" data-aos-duration="2000" alt='' />
         <p className="i-desc" data-aos="fade-right" data-aos-duration="2000" style={{color:"white"}}>
         २४ वर्षांपूर्वी महाराष्ट्राच्या वैभवशाली इतिहासाची गाथा जगभर पोहोचवण्यासाठी रंगवर्धन समितीची स्थापना करण्यात आली होती. महाराष्ट्राच्या महान इतिहासाची प्रेरणा घेऊन, रंगवर्धनने स्वाभिमानाने आणि प्रामाणिकपणे मराठी भाषा आणि मराठी परंपरांचा प्रचार व प्रसार केला आहे.
 
@@ -20,7 +20,7 @@ const Theme = () => {
       </div>
     </div>
     <div className="i-right">
-      <ReactPlayer url="https://youtu.be/8kvMtz47eAQ?si=qKyQA9TSFMR3CUEC" style={{ border: "2px solid rgb(255, 174, 66"}} className="video" controls={true}  data-aos="zoom-in" data-aos-duration="3000" />
+      <ReactPlayer url="https://vimeo.com/1022254875?share=copy#t=0" style={{ border: "2px solid rgb(255, 174, 66"}} className="video" controls={true}  data-aos="zoom-in" data-aos-duration="3000" />
     </div>
   </div>
   </>
